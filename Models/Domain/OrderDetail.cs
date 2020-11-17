3c361bf82bc929bc2db0fb57393ce03c31512c54
17dh110260 <64671767+17dh110260@users.noreply.github.com>
2020-11-17 11:27:07 +0700

    Model

---
+++ Models/Domain/OrderDetail.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeShopping.Models
+{
+    public class OrderDetail
+    {
+        public int OrderDetailID { get; set; }
+        public int OrderID { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public Order Order { get; set; }
+        public Product Product { get; set; }
+        public string HoTen { get; set; }
+        public string Email { get; set; }
+        public string SDT { get; set; }
+        public string DiaChi { get; set; }
+        public string YeuCau { get; set; }
+        public string Status { get; set; }
+
+    }
+}
